94a5ca72b722036012f8a9ba6195fd645221c097
Alex <mycatshoegazer@outlook.com>
2019-02-18 23:28:58 +0300

    Clipped icon to main form.

--- LMB Fix/MainForm.Designer.cs
+++ LMB Fix/MainForm.Designer.cs
@@ -46,6 +46,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "LMB Fix";
