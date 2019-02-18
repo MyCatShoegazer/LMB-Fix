b3755f78a81d1506292ff8d343e3923e7f9c3edd
Alex <mycatshoegazer@outlook.com>
2019-02-18 23:24:43 +0300

    Implemented double clicking fix.

---
+++ LMB Fix/GlobalHooks/NativeSupport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace LMB_Fix.GlobalHooks
+{
+    class NativeSupport
+    {
+        [DllImport("user32.dll", EntryPoint = "BlockInput")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool BlockInput([MarshalAs(UnmanagedType.Bool)] bool fBlockIt);
+
+        public static void BlockInput(TimeSpan delay)
+        {
+            try
+            {
+                BlockInput(true);
+                Thread.Sleep(delay);
+            }
+            finally
+            {
+                BlockInput(false);
+            }
+        }
+    }
+}
--- LMB Fix/MainForm.cs
+++ LMB Fix/MainForm.cs
@@ -6,15 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using LMB_Fix.GlobalHooks;
 
 namespace LMB_Fix
 {
     public partial class MainForm : Form
     {
+        private MouseHook _mouseHook;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this._mouseHook = new MouseHook();
+            this._mouseHook.LeftButtonDown += _mouseHook_LeftButtonDown;
+            this._mouseHook.LeftButtonUp += _mouseHook_LeftButtonUp;
+            this._mouseHook.Install();
+        }
+
+        private void _mouseHook_LeftButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+#if DEBUG
+            Debug.WriteLine($"LMB Up: {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+#endif
+        }
+
+        private void _mouseHook_LeftButtonDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+#if DEBUG
+            Debug.WriteLine($"LMB Down: {DateTime.Now.Second}:{DateTime.Now.Millisecond}");
+#endif
+            NativeSupport.BlockInput(new TimeSpan(0, 0, 0, 0, 20));
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
