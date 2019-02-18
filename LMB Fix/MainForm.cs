2235dc59ea210a80ce963a16d2586edd4113f6ae
Alex <mycatshoegazer@outlook.com>
2019-02-19 00:58:16 +0300

    Implemented tracking of unsaved application settings.

--- LMB Fix/MainForm.cs
+++ LMB Fix/MainForm.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LMB_Fix.GlobalHooks;
+using System;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
 using System.Windows.Forms;
-using LMB_Fix.GlobalHooks;
 
 namespace LMB_Fix
 {
     public partial class MainForm : Form
     {
         private MouseHook _mouseHook;
+
+        private bool _unsavedSettings;
 
         public MainForm()
         {
@@ -23,44 +18,50 @@ namespace LMB_Fix
 
         private void _mouseHook_RightButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
-            if(this.rightButtonFixCheckBox.Checked)
-                this.historyRichTextBox.AppendText($"RMB Up: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+            if (rightButtonFixCheckBox.Checked)
+            {
+                historyRichTextBox.AppendText($"RMB Up: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+            }
         }
 
         private void _mouseHook_RightButtonDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
-            if (this.rightButtonFixCheckBox.Checked)
+            if (rightButtonFixCheckBox.Checked)
             {
-                this.historyRichTextBox.AppendText($"RMB Down: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
-                NativeSupport.BlockInput(new TimeSpan(0, 0, 0, 0, this.rightButtonTrackBar.Value));
+                historyRichTextBox.AppendText($"RMB Down: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+                NativeSupport.BlockInput(new TimeSpan(0, 0, 0, 0, rightButtonTrackBar.Value));
             }
         }
 
         private void _mouseHook_LeftButtonUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
-            if (this.leftButtonFixCheckBox.Checked)
-                this.historyRichTextBox.AppendText($"LMB Up: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+            if (leftButtonFixCheckBox.Checked)
+            {
+                historyRichTextBox.AppendText($"LMB Up: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+            }
         }
 
         private void _mouseHook_LeftButtonDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
         {
-            if (this.leftButtonFixCheckBox.Checked)
+            if (leftButtonFixCheckBox.Checked)
             {
-                this.historyRichTextBox.AppendText($"LMB Down: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
-                NativeSupport.BlockInput(new TimeSpan(0, 0, 0, 0, this.leftButtonTrackBar.Value));
+                historyRichTextBox.AppendText($"LMB Down: {DateTime.Now.Second}s:{DateTime.Now.Millisecond}ms\n");
+                NativeSupport.BlockInput(new TimeSpan(0, 0, 0, 0, leftButtonTrackBar.Value));
             }
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
         {
-            if (this.WindowState == FormWindowState.Minimized)
-                this.Hide();
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
         }
 
         private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            this.Show();
-            this.WindowState = FormWindowState.Normal;
+            Show();
+            WindowState = FormWindowState.Normal;
         }
 
         private void exitButton_Click(object sender, EventArgs e)
@@ -70,7 +71,7 @@ namespace LMB_Fix
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            Hide();
         }
 
         private void aboutButton_Click(object sender, EventArgs e)
@@ -80,53 +81,92 @@ namespace LMB_Fix
 
         private void leftButtonTrackBar_ValueChanged(object sender, EventArgs e)
         {
-            var trackBar = sender as TrackBar;
-            this.leftButtonLabel.Text = $"{trackBar.Value}ms";
+            TrackBar trackBar = sender as TrackBar;
+            leftButtonLabel.Text = $"{trackBar.Value}ms";
         }
 
         private void rightButtonTrackBar_ValueChanged(object sender, EventArgs e)
         {
-            var trackBar = sender as TrackBar;
-            this.rightButtonLabel.Text = $"{trackBar.Value}ms";
+            TrackBar trackBar = sender as TrackBar;
+            rightButtonLabel.Text = $"{trackBar.Value}ms";
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            this._mouseHook = new MouseHook();
-            this._mouseHook.LeftButtonDown += _mouseHook_LeftButtonDown;
-            this._mouseHook.LeftButtonUp += _mouseHook_LeftButtonUp;
-            this._mouseHook.RightButtonDown += _mouseHook_RightButtonDown;
-            this._mouseHook.RightButtonUp += _mouseHook_RightButtonUp;
-            this._mouseHook.Install();
+            Properties.Settings.Default.PropertyChanged += Default_PropertyChanged;
 
-            this.leftButtonTrackBar.Value = Properties.Settings.Default.LeftButtonDelay;
-            this.rightButtonTrackBar.Value = Properties.Settings.Default.RightButtonDelay;
+            _mouseHook = new MouseHook();
+            _mouseHook.LeftButtonDown += _mouseHook_LeftButtonDown;
+            _mouseHook.LeftButtonUp += _mouseHook_LeftButtonUp;
+            _mouseHook.RightButtonDown += _mouseHook_RightButtonDown;
+            _mouseHook.RightButtonUp += _mouseHook_RightButtonUp;
+            _mouseHook.Install();
+
+            leftButtonTrackBar.Value = Properties.Settings.Default.LeftButtonDelay;
+            rightButtonTrackBar.Value = Properties.Settings.Default.RightButtonDelay;
+        }
+
+        private void Default_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _unsavedSettings = true;
         }
 
         private void leftButtonFixCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            var checkBox = sender as CheckBox;
-            this.leftButtonTrackBar.Enabled = checkBox.Checked;
+            CheckBox checkBox = sender as CheckBox;
+            leftButtonTrackBar.Enabled = checkBox.Checked;
         }
 
         private void rightButtonFixCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            var checkBox = sender as CheckBox;
-            this.rightButtonTrackBar.Enabled = checkBox.Checked;
+            CheckBox checkBox = sender as CheckBox;
+            rightButtonTrackBar.Enabled = checkBox.Checked;
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            this._mouseHook.Uninstall();
+            _mouseHook.Uninstall();
+
+            if (_unsavedSettings)
+            {
+                if (
+                    MessageBox.Show(
+                        "Application settings were changed after last time. Do you want to save theme before exit?",
+                        "Unsaved settings",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question
+                        ) == DialogResult.Yes)
+                {
+                    SaveSettings();
+                }
+            }
+        }
+
+        private void SaveSettings()
+        {
+            try
+            {
+                Properties.Settings.Default.LeftButtonFixEnabled = leftButtonFixCheckBox.Checked;
+                Properties.Settings.Default.LeftButtonDelay = leftButtonTrackBar.Value;
+                Properties.Settings.Default.RightButtonFixEnabled = rightButtonFixCheckBox.Checked;
+                Properties.Settings.Default.RightButtonDelay = rightButtonTrackBar.Value;
+                Properties.Settings.Default.Save();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show(
+                    "Please, contact to your system administrator or try again later.",
+                    "Can't save application settings!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                    );
+            }
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            Properties.Settings.Default.LeftButtonFixEnabled = this.leftButtonFixCheckBox.Checked;
-            Properties.Settings.Default.LeftButtonDelay = this.leftButtonTrackBar.Value;
-            Properties.Settings.Default.RightButtonFixEnabled = this.rightButtonFixCheckBox.Checked;
-            Properties.Settings.Default.RightButtonDelay = this.rightButtonTrackBar.Value;
-            Properties.Settings.Default.Save();
+            SaveSettings();
+            _unsavedSettings = false;
         }
     }
 }
